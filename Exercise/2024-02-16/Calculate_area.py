7987c0aeff3399b254f33dd17eaa96e48219dfd8
zhong kang ping <15979624097@163.com>
2025-02-17 22:39:31 +0800

    圆的面积和周长

--- Exercise/2024-02-16/Calculate_area.py
+++ Exercise/2024-02-16/Calculate_area.py
@@ -8,13 +8,25 @@
 @Date    ：2025/2/16 下午8:09 
 """
 import math
+
 PI = 3.14
-def calculate_area(r:int) ->int:
+
+
+def calculate_area(r: int) -> int:
     try:
-        return PI * r**2
+        return PI * r ** 2
     except ValueError as e:
         print('半径r不能小于0')
+
+
+def calculate_circumference(r: int) -> float:
+    try:
+        return PI * r * 2
+    except ValueError as e:
+        print('半径r不能小于0')
+
 
 if __name__ == '__main__':
     r = int(input('请输入要计算圆面积的半径大小：'))
     print(f'圆面积为:{calculate_area(r)}')
+    print(f'圆周长为:{calculate_circumference(r)}')
